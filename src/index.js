914d3e644b4d3c9057c81edf4f131a165aebe37c
anton <lascivolin@gmail.com>
2016-09-08 07:42:57 +0300

    deploy to s3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom'
 import App from './App'
  
 render(
-  <App url='http://localhost:3000/api/posts' pollInterval={2000} />,
+  <App url='http://54.213.196.176/api/posts' pollInterval={2000} />,
   document.getElementById('root')
 );
---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, "src"),
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
+    ]
+  },
+};
